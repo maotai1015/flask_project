dc5cae87a65a6af2b2fbccb1679f4ab798edcf23
zhangjiaxing <zhangjiaxing@xinye.com>
2023-02-23 10:43:30 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,3 @@
-from flask import Flask
-
-from app.apis import api_
-
-app = Flask(__name__)
-app.register_blueprint(api_)
+print("123")
 
 #app.run(host="0.0.0.0", port=8082)
--- app/apis.py
+++ app/apis.py
@@ -1,31 +1,10 @@
 from flask import Blueprint
-from snmp_cmds import snmpwalk
-api_ = Blueprint("v1", __name__, url_prefix="/v1")
-
-import redis
-
-redis_pool = redis.ConnectionPool(host='127.0.0.1', port= 6379, db= 0)
-redis_conn = redis.Redis(connection_pool= redis_pool)
-
-@api_.route("/hs")
-def sum():
-    return "hello! lyc"
+from flask_restful import Api, Resource
+api_bp = Blueprint("v1", __name__, url_prefix="/v1")
+api = Api(api_bp)
 
 
-@api_.route("/add")
-def add_num():
-    res = redis_conn.incr("total")
-    return str(res)
+def hs():
+    return "hello! zjx"
 
-@api_.route("/get")
-def get_num():
-    res = redis_conn.get("total")
-    if not res:
-        res = "None"
-    return res
-
-@api_.route("/del")
-def del_num():
-    res = redis_conn.decr("total")
-    return str(res)
 
